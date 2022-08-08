ae45324bc7cf484b9418034d14c2a981eee97797
niloydeb1 <niloymishu@gmail.com>
2022-08-08 14:52:04 +0600

    Fix #23 Add agent index check in update k8s objects

--- core/v1/config_map.go
+++ core/v1/config_map.go
@@ -52,6 +52,7 @@ func (obj ConfigMap) Save(extra map[string]string) error {
 			log.Println("[ERROR] Insert document:", err.Error())
 			return err
 		}
+		go AgentIndex{}.Build(obj.Obj.ObjectMeta.Labels["company"], obj.AgentName).Save()
 	} else {
 		err := obj.Update(ConfigMap{Obj: obj.findByNameAndNamespaceAndCompanyId(), AgentName: obj.AgentName}, obj.AgentName)
 		if err != nil {
@@ -147,7 +148,7 @@ func (obj ConfigMap) Update(oldObj interface{}, agent string) error {
 		log.Println("[ERROR]", err.Err())
 		return err.Err()
 	}
-
+	go AgentIndex{}.Build(obj.Obj.ObjectMeta.Labels["company"], obj.AgentName).Save()
 	return nil
 }
 
--- core/v1/pvc.go
+++ core/v1/pvc.go
@@ -51,6 +51,7 @@ func (obj PersistentVolumeClaim) Save(extra map[string]string) error {
 			log.Println("[ERROR] Insert document:", err.Error())
 			return err
 		}
+		go AgentIndex{}.Build(obj.Obj.ObjectMeta.Labels["company"], obj.AgentName).Save()
 	} else {
 		err := obj.Update(PersistentVolumeClaim{Obj: obj.findByNameAndNamespace(), AgentName: obj.AgentName}, obj.AgentName)
 		if err != nil {
@@ -145,7 +146,7 @@ func (obj PersistentVolumeClaim) Update(oldObj interface{}, agent string) error 
 		log.Println("[ERROR]", err.Err())
 		return err.Err()
 	}
-
+	go AgentIndex{}.Build(obj.Obj.ObjectMeta.Labels["company"], obj.AgentName).Save()
 	return nil
 }
 
